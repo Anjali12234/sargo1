97d38562bfbbf380da0fac4130bd19ba0dfa8502
altaafali092 <altaafali092@gmail.com>
2024-11-25 07:45:48 +0545

    commit

---
+++ app/Http/Controllers/Admin/EnquiryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Enquiry;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class EnquiryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $enquirys=Enquiry::latest()->paginate();
+        return view('admin.enquiry.index',compact('enquirys'));
+    }
+
+
+
+    public function show(Enquiry $enquiry)
+    {
+        dd($enquiry);
+        return view('admin.enquiry.show',compact('enquiry'));
+    }
+
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Enquiry $enquiry)
+    {
+        $enquiry->delete();
+        Alert::success('Enquiry message deleted successfully');
+        return back();
+    }
+}
---
+++ resources/views/admin/enquiry/Index.blade.php
@@ -0,0 +1,84 @@
+@extends('backend.layouts.master')
+@section('container')
+    <div class="min-height-200px">
+        <div class="page-header">
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <div class="title">
+                        <h4>Enquiry Messages </h4>
+                    </div>
+                    <nav aria-label="breadcrumb" role="navigation">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('admin.dashboard') }}">Home</a>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                Enquiry Messages
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="pd-20 card-box mb-30">
+            <div class="pd-20">
+                <h4 class="text-blue h4"> Enquiry Message Lists</h4>
+
+            </div>
+            <div class="pb-20">
+                <table class="data-table table stripe hover nowrap">
+                    <thead>
+                        <tr>
+                            <th class="table-plus datatable-nosort">S.No</th>
+                            <th>Full Name</th>
+                            <th>Email </th>
+                            <th>Phone </th>
+                            <th>Message </th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($enquirys as $key => $enquiry)
+                            <tr>
+                                <td class="table-plus">{{ $loop->iteration }}</td>
+
+                                <td>{{ $enquiry->full_name }}</td>
+                                <td>{{ $enquiry->email }}</td>
+                                <td>{{ $enquiry->phone_number }}</td>
+                                <td>{{ $enquiry->message }}</td>
+                                <td>
+                                    <div class="dropdown">
+                                        <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
+                                            href="#" role="button" data-toggle="dropdown">
+                                            <i class="dw dw-more"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                            <a class="dropdown-item"
+                                                href="{{ route('admin.enquiry-message.show', $enquiry) }}"><i
+                                                    class="dw dw-view"></i>view</a>
+
+                                            <form action="{{ route('admin.enquiry-message.destroy', $enquiry) }}"
+                                                method="POST" style="display: inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="dropdown-item"
+                                                    onclick="return confirm('Are You sure want to delete')"> <i
+                                                    class="dw dw-delete-3"></i>Delete </button>
+
+                                            </form>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+                {{ $enquirys->links() }}
+            </div>
+        </div>
+
+    </div>
+@endsection
