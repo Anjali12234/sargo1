ea3c8722e365cc64775af3e72fe147f2bff58bb6
anjali12234 <anjisharma649@gmail.com>
2024-11-27 01:29:00 +0545

    commit

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -155,7 +155,7 @@
                                                     Colours
                                                 </a>
                                             </div>
-                                            <div
+                                            {{-- <div
                                                 class="hs-dropdown [--strategy:static] md:[--strategy:absolute] [--adaptive:none] md:[--trigger:hover] [--is-collapse:true] md:[--is-collapse:false] relative">
                                                 <button id="hs-header-base-dropdown-sub" type="button"
                                                     class="hs-dropdown-toggle w-full flex justify-between items-center text-sm text-gray-800 rounded-lg p-2 md:px-3 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300">
@@ -184,7 +184,7 @@
                                                 </button>
 
 
-                                            </div>
+                                            </div> --}}
 
                                         </div>
                                     </div>
