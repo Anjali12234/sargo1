dd1f1710569df296191bcc6d2acc714baee9c172
anjali12234 <anjisharma649@gmail.com>
2024-12-16 16:27:23 +0545

    commit

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -5,21 +5,22 @@
     "isAutoPlay": true
   }'
     class="relative">
-    <div class="hs-carousel relative overflow-hidden w-full min-h-96 bg-white  ">
+    <div class="hs-carousel relative overflow-hidden w-full min-h-96 bg-white">
         <div
-            class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
+            class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-[2000ms] opacity-0">
             @foreach ($sliders as $slider)
                 <div class="hs-carousel-slide">
                     <img src="{{ $slider->image }}" alt="{{ $slider->title }}"
-                        class="w-full h-full object-cover ">
+                        class="w-full h-full object-cover">
                 </div>
             @endforeach
-
         </div>
     </div>
 
     <button type="button"
-        class="hs-carousel-prev hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 rounded-s-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+        class="hs-carousel-prev hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 
+        start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none
+         focus:bg-gray-800/10 rounded-s-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
         <span class="text-2xl" aria-hidden="true">
             <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -30,7 +31,9 @@
         <span class="sr-only">Previous</span>
     </button>
     <button type="button"
-        class="hs-carousel-next hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 rounded-e-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+        class="hs-carousel-next hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 end-0 
+        inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none
+         focus:bg-gray-800/10 rounded-e-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
         <span class="sr-only">Next</span>
         <span class="text-2xl" aria-hidden="true">
             <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
